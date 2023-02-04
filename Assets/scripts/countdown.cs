0bb03fd5b7d7349373015da87270e5c075388354
Michelle <mdeniselau@gmail.com>
2023-02-04 16:13:39 +0900

    added timer script

---
+++ Assets/scripts/countdown.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countdown : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
